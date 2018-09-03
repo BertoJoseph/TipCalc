5bbf283929143e26180915459f1b3125e63144bd
Starlon Frederick <Star@Starlons-MacBook-Pro.local>
2018-09-02 20:30:02 -0400

    added .ignore .gitattributes

--- TipCalc/ViewController.swift
+++ TipCalc/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TipCalc
 //
-//  Created by Starlon Frederick on 9/2/18.
+//  Created by Berto Joseph on 9/2/18.
 //  Copyright © 2018 Berto Joseph. All rights reserved.
 //
 
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPercentages = [0.10, 0.15, 0.20]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
